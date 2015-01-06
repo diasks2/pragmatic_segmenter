4b24ae64339358b9b7a3a803dc625fe2cfea6c1d
Kevin Dias <diasks2@gmail.com>
2015-01-06 22:16:36 +0900

    Add rules

---
+++ lib/pragmatic_segmenter/rules.rb
@@ -0,0 +1,38 @@
+module PragmaticSegmenter
+  module Rules
+    # Rubular: http://rubular.com/r/EUbZCNfgei
+    EmailRule = Rule.new(/(\w)(\.)(\w)/, '\1∮\3')
+
+    # Rubular: http://rubular.com/r/G2opjedIm9
+    GeoLocationRule = Rule.new(/(?<=[a-zA-z]°)\.(?=\s*\d+)/, '∯')
+
+    SingleNewLineRule = Rule.new(/\n/, 'ȹ')
+
+    ExtraWhiteSpaceRule = Rule.new(/\s{3,}/, ' ')
+
+    # Rubular: http://rubular.com/r/aXPUGm6fQh
+    QuestionMarkInQuotationRule = Rule.new(/\?(?=(\'|\"))/, 'ᓷ')
+
+    module ExclamationPointRules
+      # Rubular: http://rubular.com/r/XS1XXFRfM2
+      InQuotationRule = Rule.new(/\!(?=(\'|\"))/, 'ᓴ')
+
+      # Rubular: http://rubular.com/r/sl57YI8LkA
+      BeforeCommaMidSentenceRule = Rule.new(/\!(?=\,\s[a-z])/, 'ᓴ')
+
+      # Rubular: http://rubular.com/r/f9zTjmkIPb
+      MidSentenceRule = Rule.new(/\!(?=\s[a-z])/, 'ᓴ')
+
+      All = [ InQuotationRule, BeforeCommaMidSentenceRule, MidSentenceRule ]
+    end
+
+    module DoublePuctationRules
+      FirstRule = Rule.new(/\?!/, '☉')
+      SecondRule = Rule.new(/!\?/, '☈')
+      ThirdRule = Rule.new(/\?\?/, '☇')
+      ForthRule = Rule.new(/!!/, '☄')
+
+      All = [ FirstRule, SecondRule, ThirdRule, ForthRule ]
+    end
+  end
+end
---
+++ lib/pragmatic_segmenter/types.rb
@@ -0,0 +1,12 @@
+module PragmaticSegmenter
+  Rule = Struct.new(:pattern, :replacement)
+
+  class Text < String
+    def apply(*rules)
+      rules.flatten.each do |rule|
+        self.gsub!(rule.pattern, rule.replacement)
+      end
+      self
+    end
+  end
+end
