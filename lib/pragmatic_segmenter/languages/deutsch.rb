462ba75d0d00797b75be796f6d8003b6cd0dfe9e
airy <airydragon@gmail.com>
2015-01-06 11:11:30 +0200

    Convert some of DE numbers to rules

--- lib/pragmatic_segmenter/languages/deutsch.rb
+++ lib/pragmatic_segmenter/languages/deutsch.rb
@@ -3,29 +3,15 @@ module PragmaticSegmenter
     class Deutsch
       class Number < PragmaticSegmenter::Number
         # Rubular: http://rubular.com/r/hZxoyQwKT1
-        NUMBER_PERIOD_SPACE_REGEX = /(?<=\s[0-9]|\s([1-9][0-9]))\.(?=\s)/
+        NumberPeriodSpaceRule = Rule.new(/(?<=\s[0-9]|\s([1-9][0-9]))\.(?=\s)/, '∯')
 
         # Rubular: http://rubular.com/r/ityNMwdghj
-        NEGATIVE_NUMBER_PERIOD_SPACE_REGEX = /(?<=-[0-9]|-([1-9][0-9]))\.(?=\s)/
+        NegativeNumberPeriodSpaceRule = Rule.new(/(?<=-[0-9]|-([1-9][0-9]))\.(?=\s)/, '∯')
 
         def replace
           super
-          replace_period_related_to_numbers(@formatted_text)
-        end
-
-        private
-
-        def replace_period_related_to_numbers(txt)
-          new_text = replace_period_in_number_1(txt)
-          replace_period_in_number_2(new_text)
-        end
-
-        def replace_period_in_number_1(txt)
-          txt.gsub(NUMBER_PERIOD_SPACE_REGEX, '∯')
-        end
-
-        def replace_period_in_number_2(txt)
-          txt.gsub(NEGATIVE_NUMBER_PERIOD_SPACE_REGEX, '∯')
+          @formatted_text.apply(NumberPeriodSpaceRule).
+                          apply(NegativeNumberPeriodSpaceRule)
         end
       end
     end
