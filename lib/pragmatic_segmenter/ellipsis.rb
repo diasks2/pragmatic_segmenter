65d7a4f1203fbbcd05aebc33b1090b9196cd6902
airy <airydragon@gmail.com>
2015-01-06 09:32:46 +0200

    Convert Ellipsis to rule process

--- lib/pragmatic_segmenter/ellipsis.rb
+++ lib/pragmatic_segmenter/ellipsis.rb
@@ -5,56 +5,33 @@ module PragmaticSegmenter
   # replaces the periods.
   class Ellipsis
     # Rubular: http://rubular.com/r/i60hCK81fz
-    ELLIPSIS_3_CONSECUTIVE_REGEX = /\.\.\.(?=\s+[A-Z])/
+    ThreeConsecutiveRule = Rule.new(/\.\.\.(?=\s+[A-Z])/, '☏.')
 
     # Rubular: http://rubular.com/r/Hdqpd90owl
-    ELLIPSIS_4_CONSECUTIVE_REGEX = /(?<=\S)\.{3}(?=\.\s[A-Z])/
+    FourConsecutiveRule = Rule.new(/(?<=\S)\.{3}(?=\.\s[A-Z])/, 'ƪ')
 
     # Rubular: http://rubular.com/r/YBG1dIHTRu
-    ELLIPSIS_3_SPACE_REGEX = /(\s\.){3}\s/
+    ThreeSpaceRule = Rule.new(/(\s\.){3}\s/, '♟')
 
     # Rubular: http://rubular.com/r/2VvZ8wRbd8
-    ELLIPSIS_4_SPACE_REGEX = /(?<=[a-z])(\.\s){3}\.(\z|$|\n)/
+    FourSpaceRule = Rule.new(/(?<=[a-z])(\.\s){3}\.(\z|$|\n)/, '♝')
+
+    OtherThreePeriodRule = Rule.new(/\.\.\./, 'ƪ')
+
 
     attr_reader :text
     def initialize(text:)
-      @text = text
+      @text = Text.new(text)
     end
 
     def replace
-      replace_ellipsis(text)
-    end
-
-    private
-
-    def replace_ellipsis(txt)
       # http://www.dailywritingtips.com/in-search-of-a-4-dot-ellipsis/
       # http://www.thepunctuationguide.com/ellipses.html
-      txt = replace_3_period_ellipsis_with_spaces(txt)
-      txt = replace_4_period_ellipsis_with_spaces(txt)
-      txt = replace_4_consecutive_period_ellipsis(txt)
-      txt = replace_3_consecutive_period_ellipsis(txt)
-      replace_other_3_period_ellipsis(txt)
-    end
-
-    def replace_3_period_ellipsis_with_spaces(txt)
-      txt.gsub(ELLIPSIS_3_SPACE_REGEX, '♟')
-    end
-
-    def replace_4_period_ellipsis_with_spaces(txt)
-      txt.gsub(ELLIPSIS_4_SPACE_REGEX, '♝')
-    end
-
-    def replace_4_consecutive_period_ellipsis(txt)
-      txt.gsub(ELLIPSIS_4_CONSECUTIVE_REGEX, 'ƪ')
-    end
-
-    def replace_3_consecutive_period_ellipsis(txt)
-      txt.gsub(ELLIPSIS_3_CONSECUTIVE_REGEX, '☏.')
-    end
-
-    def replace_other_3_period_ellipsis(txt)
-      txt.gsub(/\.\.\./, 'ƪ')
+      @text.apply(ThreeSpaceRule).
+            apply(FourSpaceRule).
+            apply(FourConsecutiveRule).
+            apply(ThreeConsecutiveRule).
+            apply(OtherThreePeriodRule)
     end
   end
 end
