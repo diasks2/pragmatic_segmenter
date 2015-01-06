1736efbe65ec719072ed41d14f2a1623d3291489
airy <airydragon@gmail.com>
2015-01-06 12:56:53 +0200

    Extract rules module to a new file

--- lib/pragmatic_segmenter/languages.rb
+++ lib/pragmatic_segmenter/languages.rb
@@ -11,40 +11,9 @@ require 'pragmatic_segmenter/between_punctuation'
 require 'pragmatic_segmenter/sentence_boundary_punctuation'
 require 'pragmatic_segmenter/languages/english'
 require 'pragmatic_segmenter/languages/deutsch'
+require 'pragmatic_segmenter/rules'
 
 module PragmaticSegmenter
-  module Rules
-
-    SingleNewLineRule = Rule.new(/\n/, 'ȹ')
-
-    ExtraWhiteSpaceRule = Rule.new(/\s{3,}/, ' ')
-
-    # Rubular: http://rubular.com/r/aXPUGm6fQh
-    QuestionMarkInQuotationRule = Rule.new(/\?(?=(\'|\"))/, 'ᓷ')
-
-    module ExclamationPointRules
-      # Rubular: http://rubular.com/r/XS1XXFRfM2
-      InQuotationRule = Rule.new(/\!(?=(\'|\"))/, 'ᓴ')
-
-      # Rubular: http://rubular.com/r/sl57YI8LkA
-      BeforeCommaMidSentenceRule = Rule.new(/\!(?=\,\s[a-z])/, 'ᓴ')
-
-      # Rubular: http://rubular.com/r/f9zTjmkIPb
-      MidSentenceRule = Rule.new(/\!(?=\s[a-z])/, 'ᓴ')
-
-      All = [ InQuotationRule, BeforeCommaMidSentenceRule, MidSentenceRule ]
-    end
-
-    module DoublePuctationRules
-      FirstRule = Rule.new(/\?!/, '☉')
-      SecondRule = Rule.new(/!\?/, '☈')
-      ThirdRule = Rule.new(/\?\?/, '☇')
-      ForthRule = Rule.new(/!!/, '☄')
-
-      All = [ FirstRule, SecondRule, ThirdRule, ForthRule ]
-    end
-  end
-
   module Languages
     module Common
       include Rules
---
+++ lib/pragmatic_segmenter/rules.rb
@@ -0,0 +1,33 @@
+module PragmaticSegmenter
+  module Rules
+
+    SingleNewLineRule = Rule.new(/\n/, 'ȹ')
+
+    ExtraWhiteSpaceRule = Rule.new(/\s{3,}/, ' ')
+
+    # Rubular: http://rubular.com/r/aXPUGm6fQh
+    QuestionMarkInQuotationRule = Rule.new(/\?(?=(\'|\"))/, 'ᓷ')
+
+    module ExclamationPointRules
+      # Rubular: http://rubular.com/r/XS1XXFRfM2
+      InQuotationRule = Rule.new(/\!(?=(\'|\"))/, 'ᓴ')
+
+      # Rubular: http://rubular.com/r/sl57YI8LkA
+      BeforeCommaMidSentenceRule = Rule.new(/\!(?=\,\s[a-z])/, 'ᓴ')
+
+      # Rubular: http://rubular.com/r/f9zTjmkIPb
+      MidSentenceRule = Rule.new(/\!(?=\s[a-z])/, 'ᓴ')
+
+      All = [ InQuotationRule, BeforeCommaMidSentenceRule, MidSentenceRule ]
+    end
+
+    module DoublePuctationRules
+      FirstRule = Rule.new(/\?!/, '☉')
+      SecondRule = Rule.new(/!\?/, '☈')
+      ThirdRule = Rule.new(/\?\?/, '☇')
+      ForthRule = Rule.new(/!!/, '☄')
+
+      All = [ FirstRule, SecondRule, ThirdRule, ForthRule ]
+    end
+  end
+end
