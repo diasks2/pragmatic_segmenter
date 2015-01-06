649ce31cf70fd210aaf5528a90efc541048acb66
airy <airydragon@gmail.com>
2015-01-06 12:58:44 +0200

    Move EllipsisRules into Rules module

--- lib/pragmatic_segmenter/ellipsis.rb
+++ lib/pragmatic_segmenter/ellipsis.rb
@@ -1,30 +1,32 @@
 # -*- encoding : utf-8 -*-
 
 module PragmaticSegmenter
-  # This class searches for ellipses within a string and
-  # replaces the periods.
-  module EllipsisRules
-    # Rubular: http://rubular.com/r/i60hCK81fz
-    ThreeConsecutiveRule = Rule.new(/\.\.\.(?=\s+[A-Z])/, '☏.')
+  module Rules
+    # This class searches for ellipses within a string and
+    # replaces the periods.
+    module EllipsisRules
+      # Rubular: http://rubular.com/r/i60hCK81fz
+      ThreeConsecutiveRule = Rule.new(/\.\.\.(?=\s+[A-Z])/, '☏.')
 
-    # Rubular: http://rubular.com/r/Hdqpd90owl
-    FourConsecutiveRule = Rule.new(/(?<=\S)\.{3}(?=\.\s[A-Z])/, 'ƪ')
+      # Rubular: http://rubular.com/r/Hdqpd90owl
+      FourConsecutiveRule = Rule.new(/(?<=\S)\.{3}(?=\.\s[A-Z])/, 'ƪ')
 
-    # Rubular: http://rubular.com/r/YBG1dIHTRu
-    ThreeSpaceRule = Rule.new(/(\s\.){3}\s/, '♟')
+      # Rubular: http://rubular.com/r/YBG1dIHTRu
+      ThreeSpaceRule = Rule.new(/(\s\.){3}\s/, '♟')
 
-    # Rubular: http://rubular.com/r/2VvZ8wRbd8
-    FourSpaceRule = Rule.new(/(?<=[a-z])(\.\s){3}\.(\z|$|\n)/, '♝')
+      # Rubular: http://rubular.com/r/2VvZ8wRbd8
+      FourSpaceRule = Rule.new(/(?<=[a-z])(\.\s){3}\.(\z|$|\n)/, '♝')
 
-    OtherThreePeriodRule = Rule.new(/\.\.\./, 'ƪ')
+      OtherThreePeriodRule = Rule.new(/\.\.\./, 'ƪ')
 
-    All = [
-      ThreeSpaceRule,
-      FourSpaceRule,
-      FourConsecutiveRule,
-      ThreeConsecutiveRule,
-      OtherThreePeriodRule
-    ]
+      All = [
+        ThreeSpaceRule,
+        FourSpaceRule,
+        FourConsecutiveRule,
+        ThreeConsecutiveRule,
+        OtherThreePeriodRule
+      ]
 
+    end
   end
 end
