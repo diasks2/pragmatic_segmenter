a8dabc65f67011412984fcde773b330b9eb528ff
airy <airydragon@gmail.com>
2015-01-06 12:55:55 +0200

    Create new module/namespace for rules

--- lib/pragmatic_segmenter/languages.rb
+++ lib/pragmatic_segmenter/languages.rb
@@ -13,8 +13,42 @@ require 'pragmatic_segmenter/languages/english'
 require 'pragmatic_segmenter/languages/deutsch'
 
 module PragmaticSegmenter
+  module Rules
+
+    SingleNewLineRule = Rule.new(/\n/, 'ȹ')
+
+    ExtraWhiteSpaceRule = Rule.new(/\s{3,}/, ' ')
+
+    # Rubular: http://rubular.com/r/aXPUGm6fQh
+    QuestionMarkInQuotationRule = Rule.new(/\?(?=(\'|\"))/, 'ᓷ')
+
+    module ExclamationPointRules
+      # Rubular: http://rubular.com/r/XS1XXFRfM2
+      InQuotationRule = Rule.new(/\!(?=(\'|\"))/, 'ᓴ')
+
+      # Rubular: http://rubular.com/r/sl57YI8LkA
+      BeforeCommaMidSentenceRule = Rule.new(/\!(?=\,\s[a-z])/, 'ᓴ')
+
+      # Rubular: http://rubular.com/r/f9zTjmkIPb
+      MidSentenceRule = Rule.new(/\!(?=\s[a-z])/, 'ᓴ')
+
+      All = [ InQuotationRule, BeforeCommaMidSentenceRule, MidSentenceRule ]
+    end
+
+    module DoublePuctationRules
+      FirstRule = Rule.new(/\?!/, '☉')
+      SecondRule = Rule.new(/!\?/, '☈')
+      ThirdRule = Rule.new(/\?\?/, '☇')
+      ForthRule = Rule.new(/!!/, '☄')
+
+      All = [ FirstRule, SecondRule, ThirdRule, ForthRule ]
+    end
+  end
+
   module Languages
     module Common
+      include Rules
+
       PUNCT = ['。', '．', '.', '！', '!', '?', '？']
       PUNCT_AM = ['።', '፧', '?', '!']
       PUNCT_AR = ['?', '!', ':', '.', '؟', '،']
@@ -25,41 +59,11 @@ module PragmaticSegmenter
       PUNCT_MY = ['။', '၏', '?', '!']
       PUNCT_UR = ['?', '!', '۔', '؟']
 
-      # Rubular: http://rubular.com/r/aXPUGm6fQh
-      QuestionMarkInQuotationRule = Rule.new(/\?(?=(\'|\"))/, 'ᓷ')
-
-
-      module ExclamationPointRules
-        # Rubular: http://rubular.com/r/XS1XXFRfM2
-        InQuotationRule = Rule.new(/\!(?=(\'|\"))/, 'ᓴ')
-
-        # Rubular: http://rubular.com/r/sl57YI8LkA
-        BeforeCommaMidSentenceRule = Rule.new(/\!(?=\,\s[a-z])/, 'ᓴ')
-
-        # Rubular: http://rubular.com/r/f9zTjmkIPb
-        MidSentenceRule = Rule.new(/\!(?=\s[a-z])/, 'ᓴ')
-
-        All = [ InQuotationRule, BeforeCommaMidSentenceRule, MidSentenceRule ]
-      end
-
       # Rubular: http://rubular.com/r/NqCqv372Ix
       QUOTATION_AT_END_OF_SENTENCE_REGEX = /[!?\.][\"\'\u{201d}\u{201c}]\s{1}[A-Z]/
 
       # Rubular: http://rubular.com/r/JMjlZHAT4g
       SPLIT_SPACE_QUOTATION_AT_END_OF_SENTENCE_REGEX = /(?<=[!?\.][\"\'\u{201d}\u{201c}])\s{1}(?=[A-Z])/
-
-      SingleNewLineRule = Rule.new(/\n/, 'ȹ')
-
-      ExtraWhiteSpaceRule = Rule.new(/\s{3,}/, ' ')
-
-      module DoublePuctationRules
-        FirstRule = Rule.new(/\?!/, '☉')
-        SecondRule = Rule.new(/!\?/, '☈')
-        ThirdRule = Rule.new(/\?\?/, '☇')
-        ForthRule = Rule.new(/!!/, '☄')
-
-        All = [ FirstRule, SecondRule, ThirdRule, ForthRule ]
-      end
 
       attr_reader :text, :language, :doc_type
       def initialize(text:, **args)
