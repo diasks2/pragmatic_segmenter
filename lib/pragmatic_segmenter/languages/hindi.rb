e0b01e3a42af20b1f9d2aee42148f27713d481e9
Kevin Dias <diasks2@gmail.com>
2015-01-06 20:03:12 +0900

    Refactor sentence boundary punctuation to language classes

--- lib/pragmatic_segmenter/languages/hindi.rb
+++ lib/pragmatic_segmenter/languages/hindi.rb
@@ -1,7 +1,13 @@
 module PragmaticSegmenter
   module Languages
     class Hindi
+      class SentenceBoundaryPunctuation < PragmaticSegmenter::SentenceBoundaryPunctuation
+        SENTENCE_BOUNDARY = /.*?[।\|!\?]|.*?$/
 
+        def split
+          text.scan(SENTENCE_BOUNDARY)
+        end
+      end
     end
   end
 end
--- lib/pragmatic_segmenter/languages/persian.rb
+++ lib/pragmatic_segmenter/languages/persian.rb
@@ -1,7 +1,20 @@
 module PragmaticSegmenter
   module Languages
     class Persian
+      class SentenceBoundaryPunctuation < PragmaticSegmenter::SentenceBoundaryPunctuation
+        SENTENCE_BOUNDARY = /.*?[:\.!\?؟]|.*?\z|.*?$/
 
+        def split
+          txt = replace_non_sentence_boundary_punctuation(text)
+          txt.scan(SENTENCE_BOUNDARY)
+        end
+
+        private
+
+        def replace_non_sentence_boundary_punctuation(txt)
+          txt.gsub(/(?<=\d):(?=\d)/, '♭').gsub(/،(?=\s\S+،)/, '♬')
+        end
+      end
     end
   end
 end
