9a8fcda8d2b30b109e22a4df7870629bf5054713
airy <airydragon@gmail.com>
2015-06-02 06:39:49 +0300

    Extract Amharic specs

--- spec/pragmatic_segmenter_spec.rb
+++ spec/pragmatic_segmenter_spec.rb
@@ -59,22 +59,6 @@ RSpec.describe PragmaticSegmenter::Segmenter do
     end
   end
 
-  describe "Amharic", '(am)' do
-    context "Golden Rules" do
-      it "Sentence ending punctuation #001" do
-        ps = PragmaticSegmenter::Segmenter.new(text: "እንደምን አለህ፧መልካም ቀን ይሁንልህ።እባክሽ ያልሽዉን ድገሚልኝ።", language: 'am')
-        expect(ps.segment).to eq(["እንደምን አለህ፧", "መልካም ቀን ይሁንልህ።", "እባክሽ ያልሽዉን ድገሚልኝ።"])
-      end
-    end
-
-    describe '#segment' do
-      it 'correctly segments text #001' do
-        ps = PragmaticSegmenter::Segmenter.new(text: "እንደምን አለህ፧መልካም ቀን ይሁንልህ።እባክሽ ያልሽዉን ድገሚልኝ።", language: 'am')
-        expect(ps.segment).to eq(["እንደምን አለህ፧", "መልካም ቀን ይሁንልህ።", "እባክሽ ያልሽዉን ድገሚልኝ።"])
-      end
-    end
-  end
-
   describe "Persian", '(fa)' do
     context "Golden Rules" do
       it "Sentence ending punctuation #001" do
