594726d4a0df9c7ece1def1304f5fc5e0422e4b4
Francis Tyers <ftyers@users.noreply.github.com>
2018-07-14 14:14:16 +0200

    Update tatar.rb
    Copy/paste the handling of 1. from Danish

--- lib/pragmatic_segmenter/languages/tatar.rb
+++ lib/pragmatic_segmenter/languages/tatar.rb
@@ -10,6 +10,16 @@ module PragmaticSegmenter
         PREPOSITIVE_ABBREVIATIONS = [].freeze
         NUMBER_ABBREVIATIONS = [].freeze
       end
+      
+      # This handles the case where a dot is used to denote and ordinal (5. Juni)
+      module Numbers
+        NumberPeriodSpaceRule = Rule.new(/(?<=\s[0-9]|\s([1-9][0-9]))\.(?=\s)/, '∯')
+
+        All = [
+          Common::Numbers::All,
+          NumberPeriodSpaceRule
+        ]
+      end
 
       class AbbreviationReplacer < AbbreviationReplacer
         SENTENCE_STARTERS = [].freeze
